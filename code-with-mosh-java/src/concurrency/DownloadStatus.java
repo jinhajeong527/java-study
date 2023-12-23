5090c2f1c758b7e140db4ec19265ae3a641c8b98
jinhajeong <jinha4ever@gmail.com>
2023-12-23 13:29:40 +0900

    using synchronized block with dedicated monitor objects as parameter

--- code-with-mosh-java/src/concurrency/DownloadStatus.java
+++ code-with-mosh-java/src/concurrency/DownloadStatus.java
@@ -5,21 +5,26 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class DownloadStatus {
     private int totalBytes;
-    private Lock lock = new ReentrantLock();
+    private int totalFiles;
+
+    private Object totalBytesLock = new Object();
+    private Object totalFilesLock = new Object();
+
+
+    public int getTotalFiles() {
+        return totalFiles;
+    }
     public int getTotalBytes() {
         return totalBytes;
     }
-    public void incrementTotalBytes() {
-        lock.lock();
-        try {
-            totalBytes++;
+    public void incrementTotalFiles() {
+        synchronized (totalFilesLock) {
+            totalFiles++;
         }
-        // finally block 안에 있어야 한다.
-        // exception이 thrown 됐을 때 lock이 locked 된 상태로 있길 원하지 않기 때문이다.
-        // 다른 쓰레드가 이 코드를 실행할 수 없기 때문이다. 데드락 발생할 수 있고,
-        // our application will crash
-        finally {
-            lock.unlock();
+    }
+    public void incrementTotalBytes() {
+        synchronized (totalBytesLock) {
+            totalBytes++;
         }
     }
 }
