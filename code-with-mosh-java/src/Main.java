a83e1deaa0b27beaf2ec81d6a7d6e64475e27e62
jinhajeong <jinha4ever@gmail.com>
2024-01-03 07:40:17 +0900

    multiple type parameters practice

--- code-with-mosh-java/src/Main.java
+++ code-with-mosh-java/src/Main.java
@@ -5,7 +5,6 @@ import generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        User user = Utils.max(new User(20), new User(30));
-        System.out.println(user);
+        Utils.print(1, "Summer");
     }
 }
