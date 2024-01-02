c336d7f5367849427e3a4df8ff92825743764f93
jinhajeong <jinha4ever@gmail.com>
2024-01-03 08:07:29 +0900

    wildcard parctice

--- code-with-mosh-java/src/Main.java
+++ code-with-mosh-java/src/Main.java
@@ -1,10 +1,17 @@
 import generics.GenericList;
+import generics.Instructor;
 import generics.User;
 import generics.Utils;
 
 
 public class Main {
     public static void main(String[] args) {
-        Utils.print(1, "Summer");
+        // basic OOP
+        User user = new Instructor(10);
+        Utils.printUser(new Instructor(10));
+
+        GenericList<Instructor> users = new GenericList<>();
+//        GenericList<User> users = new GenericList<User>();
+        Utils.printUsers(users);
     }
 }
--- code-with-mosh-java/src/generics/Utils.java
+++ code-with-mosh-java/src/generics/Utils.java
@@ -8,4 +8,20 @@ public class Utils {
     public static <K, V> void print(K key, V value) {
         System.out.println(key + "=" + value);
     }
+
+    public static void printUser(User user) {
+        System.out.println(user);
+    }
+    // class CAP#1 extends User {}
+    // class Instructor extends User
+    public static void printUsers
+            (GenericList<? extends User> users) {
+        // wild card character -> unknown type
+        User x = users.get(0);
+// 추가하고 싶다면 extends 대신에 super를 쓰면 된다.
+//        users.add(new User(10));
+//        users.add(new Instructor(20));
+//        Object x = users.get(0);
+//        User user = users.get(0); --> 이건 안된
+    }
 }
