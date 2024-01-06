06351bfc9e78ca174899eef63078a441f881b60d
jinhajeong <jinha4ever@gmail.com>
2024-01-06 11:18:04 +0900

    Comparator interface practice

--- code-with-mosh-java/src/Main.java
+++ code-with-mosh-java/src/Main.java
@@ -1,24 +1,19 @@
-import collections.CollectionsDemo;
 import collections.Customer;
-import collections.ListDemo;
-import generics.GenericList;
-import generics.Instructor;
-import generics.User;
-import generics.Utils;
+import collections.EmailComparator;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
         List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("a"));
-        customers.add(new Customer("d"));
-        customers.add(new Customer("b"));
-        Collections.sort(customers);
+        customers.add(new Customer("a", "e3"));
+        customers.add(new Customer("d", "e2"));
+        customers.add(new Customer("b", "e1"));
+//        Collections.sort(customers);
+        Collections.sort(customers, new EmailComparator());
         System.out.println(customers);
     }
 }
--- code-with-mosh-java/src/collections/Customer.java
+++ code-with-mosh-java/src/collections/Customer.java
@@ -2,9 +2,20 @@ package collections;
 
 public class Customer implements Comparable<Customer> {
     private String name;
+    private String email;
 
-    public Customer(String name) {
+    public Customer(String name, String email) {
         this.name = name;
+        this.email = email;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
@@ -13,10 +24,13 @@ public class Customer implements Comparable<Customer> {
         // this == other -> 0
         // this > other -> 1
         return name.compareTo(other.name);
+        // 이름에 의해서만 비교하기 때문에 유연하지 않다.
+        // 이메일이나 주소에 따라서 비교하고 싶다면? --> Comparator
     }
 
     @Override
     public String toString() {
         return name;
     }
+
 }
