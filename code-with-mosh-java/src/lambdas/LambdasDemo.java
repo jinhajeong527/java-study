58dd11372b9b63d21b6da85702df126c87ee3427
jinhajeong <jinha4ever@gmail.com>
2024-01-08 17:12:08 +0900

    practice anoaymous inner class

--- code-with-mosh-java/src/lambdas/LambdasDemo.java
+++ code-with-mosh-java/src/lambdas/LambdasDemo.java
@@ -2,7 +2,13 @@ package lambdas;
 
 public class LambdasDemo {
     public static void show() {
-        greet(new ConsolePrinter());
+        greet(new Printer() {
+            // 익명 클래스
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
     }
     public static void greet(Printer printer) {
         printer.print("Hello World");
